2d966ccd7885b102d08e5048888f131501a33ca5
Johannes Heinen <johannes.heinen@gmail.com>
2009-05-16 18:10:23 +0200

    Regenerated docs + package

--- plugins/sfFilebasePlugin/lib/filter/sfFilebaseDirectoryFormFilter.class.php
+++ plugins/sfFilebasePlugin/lib/filter/sfFilebaseDirectoryFormFilter.class.php
@@ -1,12 +1,15 @@
 <?php
 
 /**
- * sfFilebaseDirectory filter form.
+ * This file is part of the sfFilebasePlugin package.
  *
- * @package    ##PROJECT_NAME##
- * @subpackage filter
- * @author     ##AUTHOR_NAME##
- * @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   de.optimusprime.sfFilebasePlugin
+ * @author    Johannes Heinen <johannes.heinen@gmail.com>
+ * @license   MIT license
+ * @copyright 2007-2009 Johannes Heinen <johannes.heinen@gmail.com>
  */
 class sfFilebaseDirectoryFormFilter extends BasesfFilebaseDirectoryFormFilter
 {
